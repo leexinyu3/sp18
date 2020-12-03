dc5466e28b034fb1beac50e5d1d30379de501190
leexinyu3 <61901405+leexinyu3@users.noreply.github.com>
2020-12-03 21:14:51 +0800

    Add files via upload

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,2 +1,92 @@
-package PACKAGE_NAME;public class Nbody {
+public class NBody {
+
+
+    public static void main(String[] args) {
+        double  T =  Double.parseDouble(args[0]);
+        double  dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        double Radius = NBody.readRadius(filename);
+        Planet[] p = NBody.readPlanets(filename);
+    /**
+     * Draw the background
+    **/
+        StdDraw.setScale(-Radius, Radius);
+        StdDraw.clear();
+        StdDraw.picture(0, 0, "images/starfield.jpg");
+        /**
+         * Draw the planet
+         **/
+        for (Planet var : p) {
+            var.draw();
+        }
+
+        StdDraw.enableDoubleBuffering();
+        double t = 0;
+        while (t <= T){
+            Double[] Fnetx = new Double[p.length];
+            Double[] Fnety = new Double[p.length];
+            for (int i =0;i<p.length;i++){
+                Fnetx[i] = p[i].calcNetForceExertedByX(p);
+                Fnety[i] = p[i].calcNetForceExertedByY(p);
+            }
+        //update
+            for (int i =0;i<p.length;i++) {
+                p[i].update(dt, Fnetx[i], Fnety[i]);
+            }
+            for (int i =0;i<p.length;i++)
+                p[i].draw();
+            StdDraw.show();
+            StdDraw.pause(10);
+            t +=dt;
+
+            }
+
+/**
+ * Print out the final state of the universe when time reaches T
+ */
+        StdOut.printf("%d\n", p.length);
+        StdOut.printf("%.2e\n", Radius);
+        for (int i = 0; i < p.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    p[i].xxPos, p[i].yyPos, p[i].xxVel,
+                    p[i].yyVel, p[i].mass, p[i].imgFileName);
+        }
+
+        }
+
+
+        public static double readRadius(String fileName) {
+        In in = new In(fileName);
+        int count = 0;
+        double Radius = 0;
+        while (count < 2) {
+            /* Each line has the rank of a country, then its
+             * name, then its production in metric tons, and
+             * finally the fraction of world salt output it produces. */
+            count += 1;
+            Radius = in.readDouble();
+        }
+        return Radius;
+    }
+
+    public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+
+        int count = in.readInt();
+        double radius = in.readDouble();
+        Planet[] p= new Planet[count];
+        for (int i = 0; i < count; i++) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            p[i] = new Planet(xP, yP, xV, yV, m, img);
+        }
+        return p;
+    }
+
+
 }
+
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,102 @@
-package PACKAGE_NAME;public class Planet {
-}
+
+
+public class Planet {
+
+    public double xxPos;
+    public double yyPos; //Its current y position
+    public double xxVel;//Its current velocity in the x direction
+    public double yyVel; //Its current velocity in the y direction
+    public double mass; //Its mass
+    public String imgFileName;
+
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p){
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName =p.imgFileName;
+    }
+
+    public double calcDistance(Planet p){
+        double dx = this.xxPos - p.xxPos;
+        double dy = this.yyPos - p.yyPos;
+        double r = Math.hypot(dx, dy);//返回所有参数的平方和的平方根
+        return r;
+    }
+    public double calcForceExertedBy(Planet p){
+        double r = calcDistance(p);
+        double F = 6.67e-11*this.mass*p.mass/(r*r);
+        return F;
+    }
+
+
+    public double calcForceExertedByX(Planet p) {
+        double Fx = this.calcForceExertedBy(p) * (p.xxPos - this.xxPos) / this.calcDistance(p);
+        return Fx;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double Fy = this.calcForceExertedBy(p) * (p.yyPos - this.yyPos) / this.calcDistance(p);
+        return Fy;
+    }
+
+
+    public double calcNetForceExertedByX(Planet[] p){
+        double Fnetx = 0;
+        for (int i = 0;i<p.length;i++){
+                if(this.equals(p[i]))
+                    continue;
+                else{
+                  Fnetx += this.calcForceExertedByX(p[i]);
+            }
+        }
+        return Fnetx;
+    }
+//
+
+    public double calcNetForceExertedByY(Planet[] p){
+        double Fnety = 0;
+        for (int i = 0;i<p.length;i++){
+            if(this.equals(p[i]))
+                continue;
+            else{
+                Fnety += this.calcForceExertedByY(p[i]);
+            }
+        }
+        return Fnety;
+    }
+
+    public void update(double dt, double fX, double fY){
+        double ax =  fX/this.mass;
+        double ay = fY/this.mass;
+        this.xxVel = this.xxVel + dt * ax;
+        this.yyVel = this.yyVel + dt * ay;
+        this.xxPos = this.xxPos + dt *  this.xxVel;
+        this.yyPos = this.yyPos + dt *  this.yyVel;
+
+    }
+
+    /**
+     * Draw the picture of the Body according to its position
+     * 用draw.picture方法
+     */
+    public void draw() {
+
+        StdDraw.picture(this.xxPos, this.yyPos, "images/" + this.imgFileName);
+
+
+    }
+
+
+    }
