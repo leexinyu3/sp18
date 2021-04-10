c49ea08d6d901072ea70a7aa60ca932d68ee5f07
leexinyu3 <61901405+leexinyu3@users.noreply.github.com>
2021-04-10 22:30:27 +0800

    Add files via upload

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,91 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] arr;
+    private  int[][] arr1;
+    private WeightedQuickUnionUF QU;
+    private int count = 0;
 
+
+    public Percolation(int N) {
+        if (N<=0)
+            throw new java.lang.IllegalArgumentException();
+       arr = new boolean[N][N];
+       arr1 = new int[N][N];
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                arr[i][j] = false;
+                arr1[i][j] = i*N+j;
+            }
+        }
+        QU = new WeightedQuickUnionUF(N * N + 2);
+
+        for(int j = 0;j<N;j++){
+            QU.union(arr1[0][j] , N*N);
+            QU.union(arr1[N-1][j] , N*N+1);
+        }
+
+    }
+
+
+    public boolean isOpen(int row, int col){
+        if (row < 0 || row >= arr.length || col < 0 || col >= arr.length){
+            return false;
+        }
+        return arr[row][col];
+    }
+    public void open(int row, int col){
+        count+=1;
+        if (isOpen(row,col)){
+           return ;
+        }
+        if(!isOpen(row,col))
+            arr[row][col] = true;
+
+        if (isOpen(row-1,col)){
+            QU.union((row-1)*arr.length+col,arr1[row][col]);
+        }
+        if (isOpen(row+1,col)){
+            QU.union((row+1)*arr.length+col,arr1[row][col]);
+        }
+        if (isOpen(row,col-1)){
+            QU.union(row*arr.length+col-1,arr1[row][col]);
+        }
+        if (isOpen(row,col+1)){
+            QU.union(row*arr.length+col+1,arr1[row][col]);
+        }
+
+
+    }
+    public boolean isFull(int row, int col) {
+        if (!isOpen(row,col)){
+            return false;
+        }
+        return QU.connected(arr1[row][col], arr.length* arr.length);
+    }
+    // number of open sites
+    public int numberOfOpenSites(){
+        return count;
+    }
+
+    public boolean percolates() {
+        return QU.connected(arr.length*arr.length, arr.length* arr.length+1);
+    }
+
+
+    public static void main(String[] args)   {
+        Percolation universe = new Percolation(5);
+        universe.open(3,4);
+        universe.open(2,4);
+        universe.open(2,2);
+        universe.open(2,3);
+        universe.open(0,2);
+        universe.open(1,2);
+        universe.open(4,4);
+        System.out.println(universe.isFull(0,2));
+        System.out.println(universe. percolates() );
+
+    }
 }
+
+
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,62 @@
 package hw2;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+import java.util.Random;
 
 public class PercolationStats {
+        private Random RANDDOM  = new Random(565);
+        private  double sum=0;
+        Percolation []p;
+        private double[] x;
+
+        public PercolationStats(int N, int T, PercolationFactory pf){
+                if (N <= 0 || T <= 0){
+                        throw new IllegalArgumentException("java.lang.IllegalArgumentException");
+                }
+                p = new Percolation[T];
+                x = new double[T];
+                for(int i = 0;i<T;i++){
+                        p[i] = pf.make(N);
+                        while(!p[i].percolates()){
+                                int a = RANDDOM.nextInt(N);
+                                int b = RANDDOM.nextInt(N);
+                               p[i].open(a,b);
+                        }
+                        x[i] = ((double)p[i].numberOfOpenSites())/(N*N);
+                        sum = sum + x[i];
+                }
+
+        } // perform T independent experiments on an N-by-N grid
+        public double mean() {
+                return sum/=x.length;
+
+        } // sample mean of percolation threshold
+        public double stddev()  {
+                double mean = mean();
+                double res= 0;
+                for(double xi :x){
+                       res+=(xi-mean)* (xi-mean);
+                }
+
+                return Math.sqrt(res);
+        }// sample standard deviation of percolation threshold
+        public double confidenceLow(){
+               return mean()-1.96*stddev()/Math.sqrt(x.length);
+
+        } // low endpoint of 95% confidence interval
+        public double confidenceHigh()  {
+                return mean()+1.96*stddev()/Math.sqrt(x.length);
+
+        } // high endpoint of 95% confidence interval
+
+        public static void main(String[] args){
+                PercolationFactory pf = new PercolationFactory();
+                PercolationStats ps = new PercolationStats(80,100,pf);
+                System.out.println(ps.mean());
+                System.out.println(ps.stddev());
+                System.out.println(ps.confidenceLow());
+                System.out.println(ps.confidenceHigh());
+        }
 
 }
